c2e9c33da0285375dca1dd673ed9a285179401e8
Layla <layla.hisham.sami@gmail.com>
2025-07-23 13:38:30 -0700

    Initial Commit

---
+++ interactive/interactiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveApp.swift
+//  interactive
+//
+//  Created by Layla Hisham on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct interactiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
